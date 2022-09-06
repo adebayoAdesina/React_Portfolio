f062112222771a2cea46480ff3145d00f5f2d531
adebayoAdesina <adebayoadesina789@gmail.com>
2022-09-06 16:24:12 +0100

    footer section added.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import Wave from "../assets/img/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin";
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <FooterBackground src={Wave} alt="wave" />
+      <FooterContent>
+        <span>example@gmail.com</span>
+        <Icons>
+          <Insta color="white" size="3rem" />
+          <Facebook color="white" size="3rem" />
+          <Linkedin color="white" size="3rem" />
+        </Icons>
+      </FooterContent>
+    </StyledFooter>
+  );
+};
+
+const StyledFooter = styled.div`
+  display: flex;
+  align-items: center;
+  margin: -0.5rem -3.5rem;
+  position: relative;
+  margin-top: -7rem;
+`;
+const FooterBackground = styled.img`
+  width: 100%;
+`;
+const FooterContent = styled.div`
+  position: absolute;
+  padding: 0rem 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 4rem;
+  gap: 2rem;
+  color: white;
+`;
+const Icons = styled.div`
+display: flex;
+    gap: 2rem;
+`;
+export default Footer;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Contact from '../components/Contact'
 import Experience from '../components/Experience'
+import Footer from '../components/Footer'
 import Intro from '../components/intro'
 import Navbar from '../components/navbar'
 import Portfolio from '../components/Portfolio'
@@ -19,6 +20,7 @@ const MainPage = () => {
       <Portfolio/>
       <Testimonial/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
